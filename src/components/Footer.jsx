f540b0bd06970632d0967735d4f123de9ff09cce
Richard Nyande <rich.nyande@gmail.com>
2024-07-20 16:09:25 +0100

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-5 sm:px-10 px-5'>
+        <div className='screen-max-width'>
+            <div >
+                <p className='font-semibold text-gray text-xs'>
+                Designed by: {''} 
+                <span className='underline text-blue'>
+                    Richard Nyande
+                </span>
+                </p>
+                
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
